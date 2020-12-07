efb34fa24602e5dd37bcb8aa096e05be3168e28c
Hoang Van <hoangvan696969@gmail.com>
2020-12-07 18:55:09 +0700

    bài 7

---
+++ module 1/7. Biến, kiểu dữ liệu và toán tử/thuc hanh/sudungtoantu.js
@@ -0,0 +1,22 @@
+
+    let inputwidth;
+
+    let inputheight;
+
+    inputwidth=prompt("enterthewidth");
+
+    inputheight=prompt("entertheheight");
+
+    let width=parseInt(inputwidth);
+
+    let height=parseInt(inputheight);
+
+    let area=width*height;
+
+    document.write("this area is:" + area)
+
+
+
+
+
+
